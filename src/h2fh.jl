ab32c58f32f045979eebc90ce991cbbc33b727a1
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2022-08-08 16:11:07 +0200

    Update h2fh.jl

--- src/h2fh.jl
+++ src/h2fh.jl
@@ -172,10 +172,13 @@ function step!(f0, f1, f2, f3, E3, A3, op::H2fhOperator, t, h_int)
 
     f0 .= op.u1 .+ op.u2
     f2 .= op.u1 ./ sqrt(3) .- op.u2 ./ sqrt(3)
-    f1 .= cos.(t .* real(op.partial')) .* f1 .+ sin.(t .* real(op.partial')) .* f3
-    f3 .= -sin.(t .* real(op.partial')) .* f1 .+ cos.(t .* real(op.partial')) .* f3
+    op.u1 .= cos.(t .* real(op.partial')) .* f1 .+ sin.(t .* real(op.partial')) .* f3
+    op.u2 .= -sin.(t .* real(op.partial')) .* f1 .+ cos.(t .* real(op.partial')) .* f3
 
     fft!(op.f2, 1)
     E3 .-= t .* h_int .* (1im .* k) .* vec(sum(op.f2, dims = 2)) .* dv
+
+    f1 .= op.u1
+    f3 .= op.u2
 
 end
