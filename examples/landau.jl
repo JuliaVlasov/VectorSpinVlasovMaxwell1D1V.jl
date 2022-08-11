2e0b241deaaf40c875d83a2d4793d750b2b34b22
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2022-08-11 17:01:10 +0200

    Update landau.jl

--- examples/landau.jl
+++ examples/landau.jl
@@ -8,24 +8,6 @@ using Statistics
 using VectorSpinVlasovMaxwell1D1V
 
 """
-    UniformMesh(xmin, xmax, nx)
-
-1D uniform mesh data
-"""
-struct UniformMesh
-   xmin  :: Float64
-   xmax  :: Float64
-   nx    :: Int
-   dx    :: Float64
-   x     :: Vector{Float64}
-   function UniformMesh(xmin, xmax, nx)
-      dx = (xmax - xmin) / nx
-      x  = LinRange(xmin, xmax, nx+1)[begin:end-1]     
-      new( xmin, xmax, nx, dx, x)
-   end
-end
-
-"""
     compute_rho(meshv, f)
 
 Compute charge density
@@ -52,83 +34,6 @@ function compute_e(mesh::Mesh, rho::Vector{Float64})
    real(rhok)
 end
 
-"""
-    Advection(f, p, mesh, v, nv, dt)
-
-Advection type
-
-"""
-struct Advection
-    
-    p        :: Int64 
-    step     :: Float64
-    modes    :: Vector{Float64}
-    eig_bspl :: Vector{Float64}
-    eigalpha :: Vector{Complex{Float64}}
-    
-    function Advection( p, mesh :: UniformMesh )
-        nx        = mesh.nx
-        step      = mesh.dx
-        modes     = zeros(Float64, nx)
-        modes    .= [2π * i / nx for i in 0:nx-1]
-        eig_bspl  = zeros(Float64, nx)
-        eig_bspl .= bspline(p, -div(p+1,2), 0.0)
-        for i in 1:div(p+1,2)-1
-            eig_bspl .+= bspline(p, i-(p+1)÷2, 0.0) * 2 .* cos.(i * modes)
-        end
-        eigalpha  = zeros(Complex{Float64}, nx)
-        new( p, step, modes, eig_bspl, eigalpha )
-    end
-
-    function Advection( p, mesh :: Mesh )
-        nv        = mesh.nv
-        step      = mesh.dv
-        modes     = zeros(Float64, nv)
-        modes    .= [2π * i / nv for i in 0:nv-1]
-        eig_bspl  = zeros(Float64, nv)
-        eig_bspl .= bspline(p, -div(p+1,2), 0.0)
-        for i in 1:div(p+1,2)-1
-            eig_bspl .+= bspline(p, i-(p+1)÷2, 0.0) * 2 .* cos.(i * modes)
-        end
-        eigalpha  = zeros(Complex{Float64}, nv)
-        new( p, step, modes, eig_bspl, eigalpha )
-    end
-    
-end
-
-function advection!( f, adv :: Advection, v, dt)
-    
-   f̂ = fft(f,1)
-    
-   @inbounds for j in eachindex(v)
-      alpha = dt * v[j] / adv.step
-      # compute eigenvalues of cubic splines evaluated at displaced points
-      ishift = floor(-alpha)
-      beta   = -ishift - alpha
-      fill!(adv.eigalpha,0.0im)
-      for i in -div(adv.p-1,2):div(adv.p+1,2)
-         adv.eigalpha .+= (bspline(adv.p, i-div(adv.p+1,2), beta) 
-                        .* exp.((ishift+i) * 1im .* adv.modes))
-      end
-          
-      # compute interpolating spline using fft and properties of circulant matrices
-      
-      f̂[:,j] .*= adv.eigalpha ./ adv.eig_bspl
-        
-   end
-        
-   f .= real(ifft(f̂,1))
-    
-end            
-
-function (adv :: Advection)(f    :: Array{Float64,2}, 
-                            v    :: Vector{Float64}, 
-                            dt   :: Float64)
-
-    advection!( f, adv, v, dt)
-    
-end            
-
 function landau( ϵ, kx, mesh)
     nx = mesh.nx
     nv = mesh.nv
@@ -146,21 +51,17 @@ function landau_damping(tf::Float64, nt::Int64)
   nx, nv = 128, 256
   xmin, xmax = 0.0, 4π
   vmin, vmax = -6., 6.
-  meshx = UniformMesh(xmin, xmax, nx)
-  meshv = UniformMesh(vmin, vmax, nv)
   mesh = Mesh(xmin, xmax, nx, vmin, vmax, nv)
-  mesh.v .= meshv.x
   x, v = mesh.x, mesh.v    
   dx = mesh.dx
   
   # Set distribution function for Landau damping
   ϵ, kx = 0.001, 0.5
   f = landau( ϵ, kx, mesh)
-  fᵗ = zeros(nx,nv)
     
   # Instantiate advection functions
-  adv_x = Advection(p, meshx)
-  adv_v = Advection(p, mesh)
+  adv_x = BSplineAdvection(p, mesh, dims = :x)
+  adv_v = PSMAdvection(mesh)
   
   # Set time step
   dt = tf / nt
@@ -170,9 +71,7 @@ function landau_damping(tf::Float64, nt::Int64)
   
   @showprogress 1 for it in 1:nt
         
-       transpose!(fᵗ, f)
-       adv_x(fᵗ, v, 0.5dt)
-       transpose!(f, fᵗ)
+       advection!(f, adv_x, v, 0.5dt)
 
        ρ = compute_rho(mesh, f)
        e = compute_e(mesh, ρ)
@@ -181,9 +80,7 @@ function landau_damping(tf::Float64, nt::Int64)
         
        advection!(f, adv_v, e, dt)
     
-       transpose!(fᵗ, f)
-       adv_x(fᵗ, v, 0.5dt)
-       transpose!(f, fᵗ)
+       advection!(f, adv_x, v, 0.5dt)
         
   end
                   
