6d17f4d5524f60d4be0d01702e9598bcf262b5d8
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2022-08-11 16:59:34 +0200

    Revert "conflit"
    This reverts commit c97535b572668dbdf7a0f6739e98e6c853338943.

--- src/bspline.jl
+++ src/bspline.jl
@@ -35,67 +35,78 @@ $(TYPEDEF)
 Advection to be computed on each row
 
 $(TYPEDFIELDS)
+
 """
-struct BSplineAdvection <: AbstractAdvection
+struct BSplineAdvection
+    
+    dims     :: Symbol
+    p        :: Int64 
+    step     :: Float64
+    modes    :: Vector{Float64}
+    eig_bspl :: Vector{Float64}
+    eigalpha :: Vector{Complex{Float64}}
+    fhat     :: Matrix{ComplexF64}
+    
+    function BSplineAdvection( p, mesh :: Mesh; dims = :v )
 
-    mesh::Mesh
-    p::Int
-    modes::Vector{Float64}
-    eig_bspl::Vector{Float64}
-    eigalpha::Vector{ComplexF64}
-    ft::Matrix{ComplexF64}
-
-    function BSplineAdvection(mesh; p = 3)
-
-        modes = [2pi * i / mesh.nv for i = 0:mesh.nv-1]
-        eig_bspl = zeros(mesh.nv)
-        eig_bspl .= bspline(p, -div(p + 1, 2), 0.0)
-        for i = 1:div(p + 1, 2)-1
-            eig_bspl .+= bspline(p, i - (p + 1) ÷ 2, 0.0) * 2 .* cos.(i * modes)
+        if dims == :v
+            n        = mesh.nv
+            step     = mesh.dv
+            fhat = zeros(ComplexF64, mesh.nv, mesh.nx)
+        else 
+            n        = mesh.nx
+            step     = mesh.dx
+            fhat = zeros(ComplexF64, mesh.nx, mesh.nv)
         end
-        eigalpha = zeros(ComplexF64, mesh.nv)
-        ft = zeros(ComplexF64, mesh.nv, mesh.nx)
-        new(mesh, p, modes, eig_bspl, eigalpha, ft)
 
+        modes     = zeros(Float64, n)
+        modes    .= [2π * i / n for i in 0:n-1]
+        eig_bspl  = zeros(Float64, n)
+        eig_bspl .= bspline(p, -div(p+1,2), 0.0)
+        for i in 1:div(p+1,2)-1
+            eig_bspl .+= bspline(p, i-(p+1)÷2, 0.0) * 2 .* cos.(i * modes)
+        end
+        eigalpha  = zeros(Complex{Float64}, n)
+        new( dims, p, step, modes, eig_bspl, eigalpha, fhat )
     end
-
+    
 end
 
 export advection!
 
-"""
-$(SIGNATURES)
-"""
-function advection!(f, adv::BSplineAdvection, edt)
+function advection!( f, adv :: BSplineAdvection, v, dt)
+    
+   if adv.dims == :x
+       transpose!(adv.fhat, f)
+   else
+       adv.fhat .= f
+   end
 
-    p = adv.p
-    dv :: Float64 = adv.mesh.dv
+   fft!(adv.fhat,1)
+    
+   @inbounds for j in eachindex(v)
+      alpha = dt * v[j] / adv.step
+      # compute eigenvalues of cubic splines evaluated at displaced points
+      ishift = floor(-alpha)
+      beta   = -ishift - alpha
+      fill!(adv.eigalpha,0.0im)
+      for i in -div(adv.p-1,2):div(adv.p+1,2)
+         adv.eigalpha .+= (bspline(adv.p, i-div(adv.p+1,2), beta) 
+                        .* exp.((ishift+i) * 1im .* adv.modes))
+      end
+          
+      # compute interpolating spline using fft and properties of circulant matrices
+      
+      adv.fhat[:,j] .*= adv.eigalpha ./ adv.eig_bspl
+        
+   end
+        
+   ifft!(adv.fhat,1)
+   if adv.dims == :x
+       transpose!(f, real(adv.fhat))
+   else 
+       f .= real(adv.fhat)
+   end
+    
+end            
 
-    adv.ft .= f
-    fft!(adv.ft, 1)
-
-    for j in eachindex(edt)
-
-        alpha = edt[j] / dv
-        # compute eigenvalues of cubic splines evaluated at displaced points
-        ishift = floor(-alpha)
-        beta = -ishift - alpha
-        fill!(adv.eigalpha, 0.0im)
-        for i = -div(p - 1, 2):div(p + 1, 2)
-            adv.eigalpha .+= (
-                bspline(p, i - div(p + 1, 2), beta) .*
-                exp.((ishift + i) .* 1im .* adv.modes)
-            )
-        end
-
-        # compute interpolating spline using fft and properties of circulant matrices
-
-        adv.ft[:, j] .*= adv.eigalpha ./ adv.eig_bspl
-
-    end
-
-    ifft!(adv.ft, 1)
-
-    f .= real(adv.ft)
-
-end
