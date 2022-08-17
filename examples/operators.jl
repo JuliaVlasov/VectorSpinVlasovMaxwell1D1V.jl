f1512c589d3d728e1ba114f2cca7543df2ec923b
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2022-08-17 17:09:23 +0200

    Update operators.jl

--- examples/operators.jl
+++ examples/operators.jl
@@ -16,8 +16,8 @@ import VectorSpinVlasovMaxwell1D1V: H1f!
 function operators()
 
     T = 50 # 4000  # final time
-    nx = 128  # partition of x
-    nv = 128   # partition of v
+    nx = 129  # partition of x
+    nv = 129   # partition of v
     vmin, vmax = -2.5, 2.5   # v domain size()
     ke = 1.2231333040331807  #ke
     xmin, xmax = 0, 4pi / ke  # x domain size()
@@ -28,13 +28,14 @@ function operators()
     k0 = 2.0 * ke
     ww = sqrt(1.0 + k0^2.0) # w0
     ata = 0.2
+    vth = 0.17
 
     mesh = Mesh(xmin, xmax, nx, vmin, vmax, nv)
-    adv = BSplineAdvection(mesh)
-    #adv = PSMAdvection(mesh)
+    #adv = BSplineAdvection(mesh)
+    adv = PSMAdvection(mesh)
 
     E1, E2, E3, A2, A3 = initialfields( mesh, a, ww, ke, k0)
-    f0, f1, f2, f3 = initialfunction(mesh, a, ke, ata)
+    f0, f1, f2, f3 = initialfunction(mesh, a, ke, vth, ata)
 
     Ex_energy = Float64[]
     E_energy = Float64[]
@@ -75,7 +76,7 @@ results = operators()
 
 vars = matread(joinpath(@__DIR__,"sVMEata0p2.mat"))
 
-p = plot(layout=(2,2))
+p = plot(layout=(3,2))
 plot!(p[1,1], results.time, log.(results.Ex_energy), label="julia v2")
 xlabel!(p[1,1], "Ex energy - log")
 plot!(p[2,1], results.time, log.(results.E_energy), label="julia v2")
@@ -84,10 +85,13 @@ plot!(p[1,2], results.time, log.(results.B_energy), label="julia v2")
 xlabel!(p[1,2], "B energy - log")
 plot!(p[2,2], results.time, log.(results.energy), label="julia v2")
 xlabel!(p[2,2], "energy - log")
+plot!(p[3,1], results.time, results.Sz, label="julia v2")
+xlabel!(p[3,1], "Sz")
 
 plot!(p[1,1], vec(vars["time"]), log.(vec(vars["Ex_energy"])), label="matlab", legend = :bottomright)
 plot!(p[2,1], vec(vars["time"]), log.(vec(vars["E_energy"])), label="matlab", legend = :bottom)
 plot!(p[1,2], vec(vars["time"]), log.(vec(vars["B_energy"])), label="matlab")
 plot!(p[2,2], vec(vars["time"]), log.(vec(vars["energy"])), label="matlab", legend = :bottomleft)
+plot!(p[3,1], vec(vars["time"]), log.(vec(vars["Sz"])), label="matlab")
 
 
